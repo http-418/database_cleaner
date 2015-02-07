0306e6a1f244afc7d8bd7847a2fa501e91d8c40b
Andrew Jones <andrew@jones.ec>
2015-02-06 21:52:51 -0500

    Add truncation strategy for Neo4j

--- lib/database_cleaner/neo4j/truncation.rb
+++ lib/database_cleaner/neo4j/truncation.rb
@@ -1,9 +1,38 @@
 require 'database_cleaner/neo4j/base'
-require 'database_cleaner/neo4j/deletion'
+require 'neo4j-core'
 
 module DatabaseCleaner
   module Neo4j
-    class Truncation < DatabaseCleaner::Neo4j::Deletion
+    class Truncation
+      include ::DatabaseCleaner::Neo4j::Base
+      
+      def start
+        if( @max_node_id.nil? ) 
+          @max_node_id = current_node_max
+        end
+      end
+
+      def clean
+        ::Neo4j::Transaction.run do
+          # Optional match with WHERE clause fails.
+          session._query( "MATCH (n)-[r]-(b) " +
+                          "WHERE id(n) > #{@max_node_id} " +
+                          "DELETE n,r" )
+          session._query( "MATCH (n) " +
+                          "WHERE id(n) > #{@max_node_id} " +
+                          "DELETE n" )
+        end
+
+      end
+
+      def current_node_max
+        ::Neo4j::Transaction.run do
+          session.query( 'MATCH (n) ' +
+                         'RETURN id(n) AS id ' +
+                         'ORDER BY id(n) DESC ' +
+                         'LIMIT 1' ).first.id
+        end
+      end
     end
   end
 end
